3c629698aaaa6dde6fd2bdbd8793a4bb9cfe2be1
pavlo <paihitetty444@gmail.com>
2023-11-08 11:48:55 +0100

    La version definitive

--- src/fr/btsciel/Ihm.java
+++ src/fr/btsciel/Ihm.java
@@ -8,17 +8,81 @@ import java.util.Random;
 @SuppressWarnings("ALL")
 public class Ihm {
     static Temperature [] temperatures = new Temperature[7];
+    static DecimalFormat df = new DecimalFormat("00.00");
+    static Random rd = new Random();
     public static void main(String[] args) {
         int choice = 0;
-        Random rd = new Random();
-        DecimalFormat df = new DecimalFormat("##.##");
 
+        fillTemperatures();
+
+        while (choice != 13) {
+            System.out.println("Menu...");
+            System.out.println("1: afficher tous les temperatures");
+            System.out.println("2: afficher tous les temperatures du matin");
+            System.out.println("3: afficher tous les temperatures du soir");
+            System.out.println("4: afficher le temperature maximale du matin");
+            System.out.println("5: afficher le temperature maximale du soir");
+            System.out.println("6: afficher le temperature maximale du tous temperatures");
+            System.out.println("7: afficher le temperature minimale du matin");
+            System.out.println("8: afficher le temperature minimale du soir");
+            System.out.println("9: afficher le temperature minimale du tous temperatures");
+            System.out.println("10: afficher la moyenne de temperature du matin");
+            System.out.println("11: afficher la moyenne de temperature du soir");
+            System.out.println("12: afficher la moyenne du tous temperatures");
+            System.out.println("13: sortir de programme");
+
+            System.out.print("Votre choix: ");
+            choice = In.readInteger();
+
+            switch (choice) {
+                case 1:
+                    displayAll();
+                    break;
+                case 2:
+                    displayMorning();
+                    break;
+                case 3:
+                    displayEvening();
+                    break;
+                case 4:
+                    System.out.println("Temperature maximale du matin: " + df.format(getMaxMorning()));
+                    break;
+                case 5:
+                    System.out.println("Temperature maximale du soir: " + df.format(getMaxEvening()));
+                    break;
+                case 6:
+                    System.out.println("Temperature maximale du tous temperatures: " + df.format(getMaxAll()));
+                    break;
+                case 7:
+                    System.out.println("Temperature minimale du matin: " + df.format(getMinMorning()));
+                    break;
+                case 8:
+                    System.out.println("Temperature minimale du soir: " + df.format(getMinEvening()));
+                    break;
+                case 9:
+                    System.out.println("Temperature minimale du tous temperatures: " + df.format(getMinAll()));
+                    break;
+                case 10:
+                    System.out.println("La moyenne de temperatures du matin: " + df.format(getAverageMorning()));
+                    break;
+                case 11:
+                    System.out.println("La moyenne de temperatures du soir: " + df.format(getAverageEvening()));
+                    break;
+                case 12:
+                    System.out.println("La moyenne de tous temperatures: " + df.format(getAverageAll()));
+                    break;
+                case 13:
+                    System.out.print("Vous etes sorti de programme");
+            }
+        }
+    }
+
+    public static void fillTemperatures() {
         for (int i = 0; i < temperatures.length; i++) {
             temperatures[i] = new Temperature();
             temperatures[i].matin = rd.nextDouble(20) - 10;
             temperatures[i].soir = rd.nextDouble(10, 30);
         }
-
         temperatures[0].jour = "Lundi";
         temperatures[1].jour = "Mardi";
         temperatures[2].jour = "Mercredi";
@@ -26,61 +90,67 @@ public class Ihm {
         temperatures[4].jour = "Vendredi";
         temperatures[5].jour = "Samedi";
         temperatures[6].jour = "Dimanche";
+    }
 
-        while (choice != 11) {
-            System.out.println("Menu...");
-            System.out.println("1: afficher tous les temperatures");
-            System.out.println("2: afficher le temperature maximale du matin");
-            System.out.println("3: afficher le temperature maximale du soir");
-            System.out.println("4: afficher le temperature maximale du tous temperatures");
-            System.out.println("5: afficher le temperature minimale du matin");
-            System.out.println("6: afficher le temperature minimale du soir");
-            System.out.println("7: afficher le temperature minimale du tous temperatures");
-            System.out.println("8: afficher la moyenne de temperature du matin");
-            System.out.println("9: afficher la moyenne de temperature du soir");
-            System.out.println("10: afficher la moyenne du tous temperatures");
-            System.out.println("11: sortir de programme");
-
-            System.out.print("Votre choix: ");
-            choice = In.readInteger();
-
-            switch (choice) {
-                case 1:
-                    for (int i = 0; i < temperatures.length; i++) {
-                        System.out.println(temperatures[i].jour + " Matin: " + df.format(temperatures[i].matin) + " Soir: " + df.format(temperatures[i].soir));
-                    }
-                    break;
-                case 2:
-                    System.out.println("Temperature maximale du matin: " + df.format(getMaxMorning()));
-                    break;
-                case 3:
-                    System.out.println("Temperature maximale du soir: " + df.format(getMaxEvening()));
-                    break;
-                case 4:
-                    System.out.println("Temperature maximale du tous temperatures: " + df.format(getMaxAll()));
-                    break;
-                case 5:
-                    System.out.println("Temperature minimale du matin: " + df.format(getMinMorning()));
-                    break;
-                case 6:
-                    System.out.println("Temperature minimale du soir: " + df.format(getMinEvening()));
-                    break;
-                case 7:
-                    System.out.println("Temperature minimale du tous temperatures: " + df.format(getMinAll()));
-                    break;
-                case 8:
-                    System.out.println("La moyenne de temperatures du matin: " + df.format(getAverageMorning()));
-                    break;
-                case 9:
-                    System.out.println("La moyenne de temperatures du soir: " + df.format(getAverageEvening()));
-                    break;
-                case 10:
-                    System.out.println("La moyenne de tous temperatures: " + df.format(getAverageAll()));
-                    break;
-                case 11:
-                    System.out.print("Vous etes sorti de programme");
+    public static void displayDays() {
+        System.out.print("Jour" + "\t\t");
+        for (int i = 0; i < temperatures.length; i++) {
+            System.out.print(temperatures[i].jour + "\t\t\t");
+        }
+        System.out.print("\n");
+    }
+    public static void displayAll() {
+        displayDays();
+        System.out.print("Matin" + "\t\t");
+        for (int j = 0; j < temperatures.length; j++) {
+            System.out.print(df.format(temperatures[j].matin) + "°" + "\t\t\t");
+            if (j == 2) {
+                System.out.print("\t");
+            }
+            if (j == 4) {
+                System.out.print("\t");
             }
         }
+        System.out.print("\n");
+        System.out.print("Soir" + "\t\t");
+        for (int k = 0; k < temperatures.length; k++) {
+            System.out.print(df.format(temperatures[k].soir) + "°" + "\t\t\t");
+            if (k == 2) {
+                System.out.print("\t");
+            }
+            if (k == 4) {
+                System.out.print("\t");
+            }
+        }
+        System.out.print("\n");
+    }
+    public static void displayMorning() {
+        displayDays();
+        System.out.print("Matin" + "\t\t");
+        for (int i = 0; i < temperatures.length; i++) {
+            System.out.print(df.format(temperatures[i].matin) + "°" + "\t\t\t");
+            if (i == 2) {
+                System.out.print("\t");
+            }
+            if (i == 4) {
+                System.out.print("\t");
+            }
+        }
+        System.out.print("\n");
+    }
+    public static void displayEvening() {
+        displayDays();
+        System.out.print("Soir" + "\t\t");
+        for (int i = 0; i < temperatures.length; i++) {
+            System.out.print(df.format(temperatures[i].soir) + "°" + "\t\t\t");
+            if (i == 2) {
+                System.out.print("\t");
+            }
+            if (i == 4) {
+                System.out.print("\t");
+            }
+        }
+        System.out.print("\n");
     }
     public static double getMaxMorning() {
         double max = Double.MIN_VALUE;
